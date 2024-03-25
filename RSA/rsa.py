9b107707eda217c3fa09491e1019c0e4f95e3687
susansu10 <410821204@gms.ndhu.edu.tw>
2024-03-26 00:15:50 +0800

    first commit

---
+++ RSA/rsa.py
@@ -0,0 +1,5 @@
+c = 0x4e2c1f3cdd0ad977399813d9afaa13e1eb224665f9b35f988343d348570570fb
+d = 0x314235ab3d320f8067994aa0de9c05b0bc346e83c8ba767d1684cc4b10d8aff1
+n = 0xa233271b7f7eec0e721c745aed5fb67e9d57cb5086863fd922ddae2760edd059
+m = pow(c, d, n)
+print(int.to_bytes(m, 256, 'big').decode('utf-8'))
